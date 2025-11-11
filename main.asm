280e66f2ede89a74ab6af29025ec8207a29789e4
jvanroosKDG <jvanroos@gmail.com>
2025-11-12 00:36:00 +0100

    code added

---
+++ main.asm
@@ -0,0 +1,124 @@
+!cpu 6502
+!to "main.prg", cbm 
+
+				* = $0801
+				
+				!word next
+				!word 10
+				!byte $9e
+				!text "4096"
+				!byte 0
+next    		!word 0
+
+				*=$1000
+; ----------------------------------------------------------
+; Code start
+; ----------------------------------------------------------
+
+; --- KERNAL & BASIC uitschakelen ---
+				sei
+				lda #$35
+				sta $01           ; %00110101 = RAM in plaats van KERNAL/BASIC ROM
+
+; --- IRQ vector instellen op $FFFE/$FFFF ---
+				lda #<irq
+				sta $fffe
+				lda #>irq
+				sta $ffff
+
+; --- VIC-II initialisatie ---
+				lda #$7f
+				sta $dc0d         ; CIA interrupts uit
+				sta $dd0d
+					
+					;      o-- Sprite - Sprite Interrupt 
+					;      |o-- Sprite - Background Interrupt 
+					;      ||o- Raster Interrupt
+					;      |||
+				lda #%00000111
+				sta $d01a         ; Raster interrupt inschakelen (bit 0)
+
+				lda $d011
+				and #$7f
+				sta $d011         ; bit7=0, dus raster < 256
+		
+				lda #100
+				sta $d012         ; Rasterlijn 100
+
+				lda #$0e
+				sta $d020         ; Borderkleur = cyaan
+				lda #$06
+				sta $d021         ; Achtergrondkleur = cyaan
+				
+				lda #%00000011
+				sta $d015
+				
+				lda #80
+				sta $d000
+				sta $d001
+				sta $d002
+				sta $d003
+				
+				cli               ; Interrupts weer aan
+
+; --- Eindeloze hoofdloop ---
+mainloop:
+				inc $0400
+				jmp mainloop				
+				
+irq				pha
+				txa
+				pha
+				tya
+				pha
+			;-----------------------------------
+			; Raster Interrupt Check
+			;-----------------------------------
+				lda #$01
+				and $d01a								; Raster interrupt enabled.
+				bit $d019
+				beq +
+			;-----------------------------------
+			; Raster interrupt
+			;-----------------------------------	
+				inc $07e7
+				lda #$01
+				sta $d019
+			;-----------------------------------
+			; Sprite-Background Interrupt Check
+			;-----------------------------------
++				lda #$02
+				and $d01a								;  Sprite-Background collision interrupt enabled.
+				bit $d019
+				beq +
+			;-----------------------------------
+			; Sprite <> Background Interrupt
+			;-----------------------------------
+				inc $07c0
+				lda #$02
+				sta $d019
+			;-----------------------------------
+			; Sprite Collision Interrupt Check
+			;-----------------------------------
++				lda #$04
+				and $d01a								;  Sprite-Sprite collision interrupt enabled.
+				bit $d019
+				beq +
+			;-----------------------------------
+			; Sprite Collision Interrupt
+			;-----------------------------------
+				inc $0427
+				lda #$04
+				sta $d019
+			;-----------------------------------
+			; Reset Sprite Collision Registers
+			;-----------------------------------
++				lda $d01e								;	$D01E = Sprite-sprite collision register (per bit welke sprites).
+				lda $d01f								;	$D01F = Sprite-background collision register.
+				pla
+				tay
+				pla
+				tax
+				pla
+				rti
+				
